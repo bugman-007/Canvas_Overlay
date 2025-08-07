f2a3b8ad9b15b1916f1426dcfe72089a4c79b086
bugman-007 <terryximons@gmail.com>
2025-08-07 10:59:14 -0400

    df

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <h3>Hi, Yoav! Currently My account is blocked from Upwork, I don't know why this happend.</h3>
         <h4>I am doing appeal now, and maybe I will recover the account sooner, must I can recove the account.</h4>
         <h4>If you have much time to the deadline, we can continue the project out of the platform, but if you are okay for some dealy we can continue in Upwork after my account recovered.</h4>
-        <h4>My contract information: Telegram: dmytro7070 Gmail: dkuropiatnykov@gmail.com</h4>
+        <h4>My contract information: Telegram: dmytro7070 Gmail: oleksandrpetliuk38@gmail.com</h4>
       </header>
       <main>
         <VideoEditor />
